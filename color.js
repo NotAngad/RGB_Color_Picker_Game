2a888ee5a16f4a101f5880c3e1342184dddb4008
Angad Srivastav <43061079+NotAngad@users.noreply.github.com>
2019-10-16 09:26:55 +0530

    Update color.js

--- color.js
+++ color.js
@@ -1,3 +1,10 @@
+// Title : ColorPickerGame.js
+// Author : Angad Sudesh Srivastav
+// Time : 14th October 2019 (4:31 pm)
+// Objective : To make a javascript file so the page for our color picker game is dynamic.  
+
+
+//Random Color in an array : 
 colo = ["rgb("+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+")", 
 "rgb("+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+")",
 "rgb("+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+")",
@@ -6,16 +13,15 @@ colo = ["rgb("+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 2
 "rgb("+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+", "+Math.floor(Math.random() * 256)+")",
 ];
 
-for(var i=0; i< colo.length; i++){
-    console.log(colo[i]);
-}
-
+// Selecting all squares and applying colors to them : 
 var squares = document.querySelectorAll("#squares");
 for(var i=0; i< squares.length; i++){
     squares[i].style.background = colo[i];
 
     squares[i].addEventListener("click", function(){
       var clickedcolor = this.style.background;
+        
+//         Checking if the clicked color is the same as the random color : 
       if(clickedcolor === colo[randomcolor]){
           alert("Well done. You got it right!");
       }
@@ -25,5 +31,6 @@ for(var i=0; i< squares.length; i++){
     });
 }
 
+// Picking out a random color from our array of colors : 
 var randomcolor = +Math.floor(Math.random() * 6);
 document.getElementById("randcol").textContent = colo[randomcolor];
